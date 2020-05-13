f6aed0b348f34105bfe2403d75ce9712286c3d3b
kariukbn23 <54543371+kariukbn23@users.noreply.github.com>
2020-05-12 21:11:04 -0500

    Update

--- app.js
+++ app.js
@@ -132,26 +132,34 @@ function getPlot(id) {
         Plotly.newPlot("gauge", data_gauge, layout_gauge);
       });
   }  
-// create the function to get the necessary data
+
+// Next portion of the project required for us to display the demographic info for each idea from the metadata Json object file given. 
+// First we must generate a funciton that grabs the approrpiate information and reads the json file correctly. 
+
 function getInfo(id) {
-    // read the json file to get data
+    
     d3.json("Data/samples.json").then((data)=> {
         
         // get the metadata info for the demographic panel
+        
         var metadata = data.metadata;
 
         console.log(metadata)
 
         // filter meta data info by id
+        
         var result = metadata.filter(meta => meta.id.toString() === id)[0];
 
         // select demographic panel to put data
+        
         var demographicInfo = d3.select("#sample-metadata");
         
         // empty the demographic info panel each time before getting new id info
+        
         demographicInfo.html("");
 
         // grab the necessary demographic data data for the id and append the info to the panel
+        
         Object.entries(result).forEach((key) => {   
                 demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
         });
@@ -159,26 +167,32 @@ function getInfo(id) {
 }
 
 // create the function for the change event
+
 function optionChanged(id) {
     getPlot(id);
     getInfo(id);
 }
 
 // create the function for the initial data rendering
+
 function init() {
     // select dropdown menu 
+    
     var dropdown = d3.select("#selDataset");
 
     // read the data 
+    
     d3.json("Data/samples.json").then((data)=> {
         console.log(data)
 
         // get the id data to the dropdwown menu
+        
         data.names.forEach(function(name) {
             dropdown.append("option").text(name).property("value");
         });
 
         // call the functions to display the data and the plots to the page
+        
         getPlot(data.names[0]);
         getInfo(data.names[0]);
     });
